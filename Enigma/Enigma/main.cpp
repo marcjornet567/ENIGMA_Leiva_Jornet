613bc563f7065f914a7a1163d45813755f8d0767
Alex Leiva <alex.leiva230@gmail.com>
2025-05-15 11:20:34 +0200

    Update de minus a mayusc

--- Enigma/Enigma/main.cpp
+++ Enigma/Enigma/main.cpp
@@ -2,6 +2,7 @@
 
 int main() {
 	short opcioMenu;
+	string missatge; 
 
 	cout << "\tEnigma" << endl;
 	cout << "-----------------------" << endl;
@@ -15,22 +16,44 @@ int main() {
 	ofstream roto2;
 	ofstream roto3;
 
+	roto1.open("rotor1.txt");
+	roto1 << "MBRPJLZFOVNCKQDEWAGUTXHYS\nI" << endl;
+	roto1.close();
+
+	roto2.open("rotor2.txt");
+	roto2 << "XHSGMLDYNVBOIWZCAFJPRQKUE\nT" << endl;
+	roto2.close();
+
+	roto3.open("rotor3.txt");
+	roto3 << "TUDCSMHFALIPEWJYZNGOQBKRV\nX" << endl;
+	roto2.close();
+
 	// Opcion 1: Xifrar missatges
 	switch (opcioMenu)
 	{
 	case 1:
 		cout << "Xifrar missatges" << endl;
-		roto1.open("rotor1.txt");
-		roto1 << "MBRPJLZFOVNCKQDEWAGUTXHYSI" << endl;
-		roto1.close();
-	
-		roto2.open("rotor2.txt");
-		roto2 << "XHSGMLDYNVBOIWZCAFJPRQKUET" << endl;
-		roto2.close();
 
-		roto3.open("rotor3.txt");
-		roto3 << "TUDCSMHFALIPEWJYZNGOQBKRVX" << endl;
-		roto2.close();
+		cout << "Escriu el missatge a xifrar: ";
+		cin >> missatge; 
+
+		
+		for (int i = 0; i < missatge.length(); i++) {
+			if (missatge[i] >= 65 && missatge[i] <= 90) {
+			}
+			else if (missatge[i] >= 97 && missatge[i] <= 122) { 
+				missatge[i] -= 32;
+			}
+			else if (missatge[i] == 32) {
+				missatge[i] = ' '; 
+			}
+			else {
+				cout << "El caracter introducido no es una letra aceptable" << endl;
+				break;
+			}
+		}
+
+		// aqui hay que pasar el mensaje al archivo con getline para que pille los espacios
 
 		break;
 	case 2: 
